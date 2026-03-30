5e26166990fad1603d246add89c765588f08a677
insipido <43050662+insipido@users.noreply.github.com>
2026-03-30 13:46:02 +1000

    Delete units.js

--- units.js
+++
@@ -1,8 +0,0 @@
-export function toSI(value, type, system) {
-  if (system === "Imperial") {
-    if (type === "length") return value * 0.3048;
-    if (type === "force") return value * 4.44822;
-    if (type === "moment") return value * 1.35582;
-  }
-  return value;
-}
